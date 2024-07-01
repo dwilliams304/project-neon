499f457859d1f378657385bb631b72bee6dbdf3e
dwilliams304 <d.williams0434@gmail.com>
2024-06-30 19:04:29 -0700

    start corruption system class

---
+++ Assets/Scripts/Systems/CorruptionManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CorruptionTier {
+    Tier1,
+    Tier2,
+    Tier3,
+    Tier4,
+    Tier5,
+    Tier6
+}
+
+public class CorruptionManager : MonoBehaviour
+{
+    public static CorruptionManager Instance;
+
+
+    [SerializeField] private int currentCorruptionAmount;
+    [SerializeField] private int corruptionToNextTier;
+
+    [SerializeField] private AnimationCurve corruptionScaler;
+
+    [SerializeField] private CorruptionTier corruptionTier;
+
+
+    public delegate void OnCorruptionAmountChange(int amount);
+    public OnCorruptionAmountChange onCorruptionAmountChange;
+
+    public delegate void OnCorruptionTierChange(CorruptionTier tier);
+    public OnCorruptionTierChange onCorruptionTierChange;
+
+
+
+    private void Awake(){
+        Instance = this;
+    }
+
+
+    private void Start(){
+        currentCorruptionAmount = 0;
+
+    }
+
+
+    public void AddCorruption(int amount){
+        currentCorruptionAmount += amount;
+        if(currentCorruptionAmount >= corruptionToNextTier){
+            int overflow = currentCorruptionAmount - corruptionToNextTier;
+            ChangeCorruptionTier(corruptionTier, overflow);
+        }
+        onCorruptionAmountChange?.Invoke(currentCorruptionAmount);
+    }
+
+
+
+    private void ChangeCorruptionTier(CorruptionTier currentTier, int newCorruptionAmount){
+        
+    }
+}
