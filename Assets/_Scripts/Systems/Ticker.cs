8f2491e42f692107e07fb0cf49acd87d9987199b
dwilliams304 <d.williams0434@gmail.com>
2024-07-27 12:27:29 -0700

    wip

--- Assets/_Scripts/Systems/Ticker.cs
+++ Assets/_Scripts/Systems/Ticker.cs
@@ -2,26 +2,21 @@ using UnityEngine;
 
 public class Ticker : MonoBehaviour
 {
-    public static Ticker Instance;
-
     [SerializeField] private float tickTimer;
-    private float lastTick;
 
     public delegate void OnNormalTick();
     public static OnNormalTick onNormalTick;
 
     public delegate void OnSecondTick();
-    public OnSecondTick onSecondTick;
+    public static OnSecondTick onSecondTick;
 
     public delegate void OnFifthTick();
     public static OnFifthTick onFifthTick;
 
+    private float lastTick;
     private int tick;
 
-    private void Awake(){
-        Instance = this;
-        tick = 0;
-    }
+    private void Start() => tick = 0;
     
     private void Update(){
         if(Time.time >= lastTick + tickTimer){
--- Assets/_Scripts/Utility/DespawnAfterTime.cs
+++ Assets/_Scripts/Utility/DespawnAfterTime.cs
@@ -2,22 +2,14 @@ using UnityEngine;
 
 public class DespawnAfterTime : MonoBehaviour
 {
-
     [SerializeField] private float waitTime;
     [SerializeField] private bool destroyObject;
 
     private float timeActivated;
 
+    private void OnEnable() => timeActivated = Time.time;
 
-    private void OnEnable() {
-        timeActivated = Time.time;
+    private void Update() {
+        if(Time.time > timeActivated + waitTime) gameObject.SetActive(false);
     }
-
-
-    private void Update(){
-        if(Time.time > timeActivated + waitTime){
-            gameObject.SetActive(false);
-        }
-    }
-
 }
